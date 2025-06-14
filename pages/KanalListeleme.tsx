56cc6b10ad317662b0191246e65497232a33936e
getdachopa2 <yilmazoge02@gmail.com>
2025-06-14 23:47:11 +0300

    Update KanalListeleme component via AI

--- pages/KanalListeleme.tsx
+++ pages/KanalListeleme.tsx
@@ -1,1 +1,29 @@
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
+const KanalListeleme = ({ kanallar }) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>İsim</TableCell>
+            <TableCell>Açıklama</TableCell>
+            <TableCell>Durum</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {kanallar.map((kanal) => (
+            <TableRow key={kanal.id}>
+              <TableCell>{kanal.isim}</TableCell>
+              <TableCell>{kanal.aciklama}</TableCell>
+              <TableCell>{kanal.durum}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default KanalListeleme;
